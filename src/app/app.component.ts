e083c68dd1a4b756ead822640f4cf8424c1797df
Luan Marques <luan.marques@autoglass.com.br>
2021-08-13 18:38:24 -0300

    until session five

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencias : any = [];
+
+  transferir($event : any){
+     console.log($event);
+     const transferencia = {...$event, data : new Date()};
+     this.transferencias.push(transferencia);
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,12 +1,31 @@
 import { style } from '@angular/animations';
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transparencia.component.scss']
-
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transparencia.component.scss'],
 })
-    export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
-    }
+  @Output() aoTranferir = new EventEmitter<any>();
+
+
+  valor:number = 0;
+  destino:number = 0;
+
+  transferir(){
+    console.log('Nova transferencia solicitada');
+    //console.log("Valor: " + this.valor)
+    //console.log("Destino: " +this.destino)
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTranferir.emit(valorEmitir);
+    this.limparCampos();
+
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
