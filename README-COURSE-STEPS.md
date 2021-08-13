4ecef6daf9d955dffc16d0bf9c62a68548aee5bd
Luan Marques <luan.marques@autoglass.com.br>
2021-08-12 21:30:05 -0300

    angular. creating a project and first component

---
+++ README-COURSE-STEPS.md
@@ -0,0 +1,7 @@
+# Angular
+
+-Instalar e utilizar o Angular CLI;
+- Criar uma aplicação Angular do zero;
+- Utilizar o próprio CLI para levantar um servidor de desenvolvimento;
+- Como é organizada uma aplicação Angular;
+- Criar e utilizar um componente Angular.
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,12 @@
+import { style } from '@angular/animations';
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl:'./nova-transferencia.component.html',
+    styleUrls:['./nova-transparencia.component.scss']
+
+})
+    export class NovaTransferenciaComponent{
+
+    }
